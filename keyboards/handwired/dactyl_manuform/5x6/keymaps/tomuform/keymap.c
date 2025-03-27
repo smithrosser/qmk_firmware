03f8e82802960e12fb8da61d95e9ab542a453cd8
Tom Smithrosser <tom@toaster>
2025-03-27 16:12:27 +0000

    add reset to both keyboards, socd disable/enable

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/tomuform/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/tomuform/keymap.c
@@ -8,22 +8,40 @@
 
 #include "features/socd_cleaner.h"
 
-socd_cleaner_t socd_v = {{KC_E, KC_D}, SOCD_CLEANER_LAST};
-socd_cleaner_t socd_h = {{KC_S, KC_F}, SOCD_CLEANER_LAST};
+enum my_keycodes {
+    SOCD_DISABLE = SAFE_RANGE,
+    SOCD_ENABLE,
+};
+
+bool           socd_cleaner_enabled = true; // SOCD cleaning enabled
+socd_cleaner_t socd_v               = {{KC_E, KC_D}, SOCD_CLEANER_LAST};
+socd_cleaner_t socd_h               = {{KC_S, KC_F}, SOCD_CLEANER_LAST};
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-  if (!process_socd_cleaner(keycode, record, &socd_v)) { return false; }
-  if (!process_socd_cleaner(keycode, record, &socd_h)) { return false; }
+    switch (keycode) {
+        case SOCD_DISABLE:
+            socd_cleaner_enabled = false;
+            break;
+        case SOCD_ENABLE:
+            socd_cleaner_enabled = true;
+            break;
+        default:
+            break;
+    }
+    if (!process_socd_cleaner(keycode, record, &socd_v)) return false;
+    if (!process_socd_cleaner(keycode, record, &socd_h)) return false;
 
-  return true;
+    return true;
 }
 
 // Keymap
 // -----------------------------------------------------------------------------
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_5x6(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, MO(1), KC_SPC, KC_ENT, MO(2), KC_BSPC, KC_LSFT, KC_RSFT, KC_LALT, KC_DEL, KC_PGUP, KC_PGDN, KC_LGUI),
     [1] = LAYOUT_5x6(KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_NO, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, CW_TOGG, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [2] = LAYOUT_5x6(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD, KC_NO, KC_NO, KC_NO, KC_NO, MO(3), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
     [3] = LAYOUT_5x6(QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
+// clang-format off
